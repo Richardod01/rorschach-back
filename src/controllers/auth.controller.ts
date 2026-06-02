a6496364c822fd13d8f7449247042c27c6fee121
Julio Cesar <julio.alvarez@vpremier.com>
2026-06-01 18:33:53 -0600

    add format in auth controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,21 +1,33 @@
-import { Request, Response } from 'express';
-import { AuthService } from '../services/auth.service';
+import { Request, Response } from "express";
+import { AuthService } from "../services/auth.service";
 
 export const register = async (req: Request, res: Response): Promise<void> => {
   try {
     const newUser = await AuthService.registerDoctor(req.body);
-    res.status(201).json({ message: 'Doctor registrado exitosamente', user: newUser });
+    res
+      .status(201)
+      .json({ message: "Doctor registrado exitosamente", user: newUser });
   } catch (error: any) {
-    res.status(400).json({ message: 'Error al registrar', error: error.message });
+    res
+      .status(400)
+      .json({ message: "Error al registrar", error: error.message });
   }
 };
 
-export const registerAdmin = async (req: Request, res: Response): Promise<void> => {
+export const registerAdmin = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
   try {
     const newUser = await AuthService.registerAdmin(req.body);
-    res.status(201).json({ message: 'Administrador registrado exitosamente', user: newUser });
+    res.status(201).json({
+      message: "Administrador registrado exitosamente",
+      user: newUser,
+    });
   } catch (error: any) {
-    res.status(400).json({ message: 'Error al registrar', error: error.message });
+    res
+      .status(400)
+      .json({ message: "Error al registrar", error: error.message });
   }
 };
 
@@ -23,10 +35,13 @@ export const login = async (req: Request, res: Response): Promise<void> => {
   try {
     const data = await AuthService.login(req.body);
     res.status(200).json({
-      message: 'Login exitoso',
+      message: "Login exitoso",
+      code: 0,
       ...data,
     });
   } catch (error: any) {
-    res.status(401).json({ message: 'Error en el login', error: error.message });
+    res
+      .status(401)
+      .json({ message: "Error en el login", error: error.message });
   }
 };
